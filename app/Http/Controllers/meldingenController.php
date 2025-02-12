dc58275f824178bdebaef53f124c0f1074e4e23e
Yoran <49372904+Y0RAN@users.noreply.github.com>
2025-02-12 11:21:42 +0100

    opdracht klaar

--- app/Http/Controllers/meldingenController.php
+++ app/Http/Controllers/meldingenController.php
@@ -2,16 +2,36 @@
 
 //Variabelen vullen
 $attractie = $_POST['attractie'];
+$type = $_POST['type'];
 $capaciteit = $_POST['capaciteit'];
 $melder = $_POST['melder'];
+$overig = $_POST['overig'];
+
+if (isset($_POST['prioriteit']))
+{
+    $prioriteit = 1;
+}
+else 
+{
+    $prioriteit = 0;
+}
 
 echo $attractie . " / " . $capaciteit . " / " . $melder;
 
 //1. Verbinding
 require_once '../../../config/conn.php';
 
-//2. Query
+$query = "INSERT INTO meldingen (attractie, type, capaciteit, prioriteit, melder, overige_info) 
+VALUES (:attractie, :type, :capaciteit, :prioriteit, :melder, :overige_info);";
 
-//3. Prepare
+$statement = $conn->prepare($query);
 
-//4. Execute
+$statement->execute([
+    ":attractie" => $attractie,
+    ":type" => $type,
+    ":capaciteit" => $capaciteit,
+    ":prioriteit" => $prioriteit,
+    ":melder" => $melder,
+    ":overige_info" => $overig,
+]);
+?>
--- resources/views/meldingen/create.php
+++ resources/views/meldingen/create.php
@@ -22,15 +22,31 @@
             </div>
             <div class="form-group">
                 <label for="type">Type</label>
-                <!-- hier komt een dropdown -->
+                <select name="type">
+                    <option value=""> - kies een type - </option>
+                    <option value="achtbaan">achtbaan</option>
+                    <option value="water">water</option>
+                    <option value="draaiend">draaiend</option>
+                    <option value="horeca">horeca</option>
+                    <option value="kinder">kinder</option>
+                </select>
             </div>
             <div class="form-group">
                 <label for="capaciteit">Capaciteit p/uur:</label>
                 <input type="number" min="0" name="capaciteit" id="capaciteit" class="form-input">
             </div>
             <div class="form-group">
+                <label for="prioriteit">Prioriteit:</label>
+                <input type="checkbox" name="prioriteit">
+            </div>
+            <div class="form-group">
                 <label for="melder">Naam melder:</label>
                 <input type="text" name="melder" id="melder" class="form-input">
+            </div>
+
+            <div class="form-group">
+                <label for="overig">Overig:</label>
+                <textarea name="overig" rows=4 class="form-input"></textarea>
             </div>
 
             <input type="submit" value="Verstuur melding">
